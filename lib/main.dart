5727c1fb077cfc2dc9f90831b12b8db80dad071b
AntonioGarcia12 <antonio.garcia.harto@gmail.com>
2025-02-03 11:42:05 +0100

    Añadido main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eventify/config/theme/app_theme.dart';
 import 'package:eventify/config/theme/router/app_router.dart';
+import 'package:eventify/presentacion/services/notificacion_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey: 'AIzaSyA0j_toLFCavNPuxbzb38FWlYF-BE2uqCY',
+          appId: '1:792265517367:android:a1cb47ccf65fa29b9152b2',
+          messagingSenderId: '792265517367',
+          projectId: 'eventify-b6cd1',
+        ))
+      : await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: false,
+  );
+
+  final notificationService = NotificationService();
+  await notificationService.setupFirebaseMessaging();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
